f2e420b167f42ec7f980cb2eb1f8773b5a44bab0
OlgaGushchyna <106448513+OlgaGushchyna@users.noreply.github.com>
2022-08-18 11:18:52 +0300

    Create project dictionary

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
