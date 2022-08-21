38eca1f674a9a5da7e2a49ec59ad228ad1d13a38
OlgaGushchyna <106448513+OlgaGushchyna@users.noreply.github.com>
2022-08-21 17:27:57 +0300

    Add Meaning.js, Results.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
+  let [results, setResults] = useState(null);
   function handleResponce(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function handleSubmit(event) {
@@ -18,6 +20,7 @@ export default function Dictionary() {
       <form onSubmit={handleSubmit}>
         <input type="search" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
