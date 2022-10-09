f250b520516b2a886d67a2b71962bd82bd2a2d13
OlgaGushchyna <106448513+OlgaGushchyna@users.noreply.github.com>
2022-10-09 08:44:09 +0300

    Styling the app

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,26 +1,43 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import "./Dictionary.css";
 
-export default function Dictionary() {
+export default function Dictionary(props) {
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
   function handleResponce(response) {
     setResults(response.data[0]);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    let keyword = event.target[0].value;
-    let urlApi = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  function search(keyword) {
+    let urlApi = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(urlApi).then(handleResponce);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={handleSubmit}>
-        <input type="search" />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(event.target[0].value);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="search" defaultValue={props.defaultKeyword} />
+          </form>
+          <div className="hint">
+            <small> suggested words: forest, yoga, wine, cheese...</small>
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    setLoaded(true);
+    search(props.defaultKeyword);
+    return "Loading...";
+  }
 }
---
+++ src/Example.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Example.css";
+
+export default function Example(props) {
+  if (props.example) {
+    return (
+      <div className="Example">
+        <em>{props.example}</em>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
